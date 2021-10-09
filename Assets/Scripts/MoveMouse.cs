3008cf79aa0815a6d9c0014b7e4c33b2b5a07d67
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-09 14:22:33 +0700

    add problem 5

---
+++ Assets/Scripts/MoveMouse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMouse : MonoBehaviour
+{
+    public float speed = 5;
+    private float minX= -9, maxX=9, minY=-5, maxY=5;
+    private Vector2 mosPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mosPos = Input.mousePosition;
+        mosPos = Camera.main.ScreenToWorldPoint(mosPos);
+
+        if (mosPos.x > minX && mosPos.x < maxX && mosPos.y > minY && mosPos.y < maxY)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mosPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
