4c56b0fa600c820b03401945c3824ede3284be40
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-09 14:11:07 +0700

    add problem 4

---
+++ Assets/Scripts/MoveKeyboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKeyboard : MonoBehaviour
+{
+    private float speed = 3;
+    private float horizontal;
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime, 0);
+    }
+}
