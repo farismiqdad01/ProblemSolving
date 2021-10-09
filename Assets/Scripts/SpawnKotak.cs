4a950f9b0572066382e959d0c8be4d859a8f0ef7
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-10 05:20:25 +0700

    add problem 6

---
+++ Assets/Scripts/SpawnKotak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKotak : MonoBehaviour
+{
+    public float minX, maxX, minY, maxY; //minimal dan maximal batas layar dari spawn kotak
+    public GameObject kotakPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int objectCount = Random.Range(5,10); //memberikan nilai random munculnya kotak
+        for (int i = 0; i < objectCount; i++)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+            Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+    }
+}
