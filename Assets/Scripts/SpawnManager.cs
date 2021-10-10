24db788078ceaaf853c16b8e5d6da843911e0e9a
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-10 19:12:11 +0700

    add problem 8

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     public float minX, maxX, minY, maxY;
     public GameObject kotakPrefabs;
+    public GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,8 +35,8 @@ public class SpawnManager : MonoBehaviour
             float randomX = Random.Range(minX, maxX);
             float randomY = Random.Range(minY, maxY);
 
-            GameObject Meteor = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
-            Meteor.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
+            GameObject kotak = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
+            kotak.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
         }
         yield return new WaitForSeconds(3);
         StartCoroutine(SpawnKotak());
