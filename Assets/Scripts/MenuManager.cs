bb8c9fd01c619f9b772879e238578c740e3090c2
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-10 23:04:21 +0700

    add problem 10

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private void Update()
+    {
+        string scenename = SceneManager.GetActiveScene().name;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (scenename != "Problem10")
+            {
+                SceneManager.LoadScene("Problem10");
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void SelectScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ Assets/Scripts/Problem7Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Problem7Controller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    private float minX = -9, maxX = 9, minY = -5, maxY = 5;
+    private Vector2 mosPos;
+    public Text scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        mosPos = Input.mousePosition;
+        mosPos = Camera.main.ScreenToWorldPoint(mosPos);
+        scoreText.text = "Score : " + score.ToString();
+    }
+    void FixedUpdate()
+    {
+
+
+        if (mosPos.x > minX && mosPos.x < maxX && mosPos.y > minY && mosPos.y < maxY)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mosPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            return;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "kotak")
+        {
+            score++;
+        }
+    }
+
+}
