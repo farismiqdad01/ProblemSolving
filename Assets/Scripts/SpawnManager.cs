2ccdd9ea40c792c1df0865c723b2753d1cde51ed
farismiqdad01 <farismiqdad01@gmail.com>
2021-10-10 19:00:18 +0700

    add problem 8

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float minX, maxX, minY, maxY;
+    public GameObject kotakPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int objectCount = 10;
+        for (int i = 0; i < objectCount; i++)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+
+            GameObject kotak = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
+            kotak.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
+        }
+        StartCoroutine(SpawnKotak());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnKotak()
+    {
+        if (transform.childCount < 10)
+        {
+            float randomX = Random.Range(minX, maxX);
+            float randomY = Random.Range(minY, maxY);
+
+            GameObject Meteor = Instantiate(kotakPrefabs, new Vector2(randomX, randomY), Quaternion.identity) as GameObject;
+            Meteor.transform.SetParent(GameObject.FindGameObjectWithTag("Spawn").transform, false);
+        }
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SpawnKotak());
+    }
+}
